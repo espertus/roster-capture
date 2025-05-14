fb3ebc9d0c7a3f383c66ba8373e798ac21207ff5
espertus <espertus@users.noreply.github.com>
2025-05-14 14:36:25 -0700

    Create a single fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,21 +11,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safe.args)
+//    alias(libs.plugins.kotlin.compose)
 }
-
-//plugins {
-//    id("com.android.application")
-//    id("org.jetbrains.kotlin.android")
-//}
 
 android {
     namespace = "com.ellenspertus.qroster"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ellenspertus.qroster"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -48,29 +44,21 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-//    buildFeatures {
-//        compose = true
-//    }
+    buildFeatures {
+        compose = false
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
-
+    implementation(libs.androidx.navigation.ui.ktx)
+    // Test dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
